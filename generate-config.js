7a7ccf2f7239148697dbd8b0264d128f31a44076
Pavan8023 <pavanpatil8023@gmail.com>
2025-07-29 19:12:25 +0530

    casa

---
+++ generate-config.js
@@ -0,0 +1,14 @@
+import fs from "fs";
+
+const configContent = `window.env = {
+  FIREBASE_API_KEY: "${process.env.FIREBASE_API_KEY}",
+  FIREBASE_AUTH_DOMAIN: "${process.env.FIREBASE_AUTH_DOMAIN}",
+  FIREBASE_PROJECT_ID: "${process.env.FIREBASE_PROJECT_ID}",
+  FIREBASE_STORAGE_BUCKET: "${process.env.FIREBASE_STORAGE_BUCKET}",
+  FIREBASE_MESSAGING_SENDER_ID: "${process.env.FIREBASE_MESSAGING_SENDER_ID}",
+  FIREBASE_APP_ID: "${process.env.FIREBASE_APP_ID}",
+  FIREBASE_MEASUREMENT_ID: "${process.env.FIREBASE_MEASUREMENT_ID}"
+};`;
+
+fs.writeFileSync("./public/config.js", configContent);
+console.log("config.js generated successfully");
